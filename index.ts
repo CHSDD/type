90dad2759622362b91f53ab40ac709c97f9c43df
Henry <ark1159@daum.net>
2023-01-09 17:57:53 +0900

    2

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
-const nico = {
-    nickname: "nick"
+abstract class User {
+    constructor(
+        private firstName:string,
+        private lastName:string,
+        public nickname:string
+    ) {}
+    getFullName(){
+        return '${this.firstName} ${this.lastName}'
+    }
 }
+
+class Player extends User {
+
+}
+
+const nico = new Player("nico", "las", "니꼬")
